6df851ff6701cd8a05aa53d1ca0a7ce3d0efe99f
Joshua Shaffer <joshua@joshuatshaffer.com>
2026-09-05 21:40:30 -0500

    Reference the favicon of the host site to prevent requests to `/favicon.ico`.

--- src/pages/+config.ts
+++ src/pages/+config.ts
@@ -4,4 +4,7 @@ import type { Config } from "vike/types";
 export default {
   title: "Satellite View 3D",
   extends: [vikeReact],
+  // Reference the favicon of the host site to prevent requests to
+  // `/favicon.ico`.
+  favicon: "/images/favicon.png",
 } satisfies Config;
